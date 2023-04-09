f544337cfb3b0a4e34f50c496927c828ce3b6cde
Janko Tomsic <janko.tomsic@dlabs.si>
2023-04-09 05:21:07 +0200

    Cleanup token count code

--- count_tokens.py
+++
@@ -1,9 +0,0 @@
-import sys
-from tiktoken import Tokenizer, TokenCount
-from tiktoken.tokenizer import Tokenizer, RegexTokenizer
-
-text = sys.stdin.read()
-tokenizer = RegexTokenizer()
-
-token_count = sum(1 for _ in tokenizer.tokenize(text))
-print(token_count)
--- utils/textUtils.js
+++ utils/textUtils.js
@@ -22,34 +22,6 @@ function splitTextIntoChunks(text, maxLength) {
   return chunks;
 }
 
-function countTokens(text) {
-  return new Promise((resolve, reject) => {
-    const pythonProcess = spawn("python", ["count_tokens.py"]);
-
-    pythonProcess.stdin.write(text);
-    pythonProcess.stdin.end();
-
-    let tokenCount = "";
-
-    pythonProcess.stdout.on("data", (data) => {
-      tokenCount += data.toString();
-    });
-
-    pythonProcess.stderr.on("data", (data) => {
-      console.error(`Error: ${data.toString()}`);
-      reject(data.toString());
-    });
-
-    pythonProcess.on("close", (code) => {
-      if (code !== 0) {
-        reject(`Python process exited with code ${code}`);
-      } else {
-        resolve(parseInt(tokenCount.trim()));
-      }
-    });
-  });
-}
-
 module.exports = {
   splitTextIntoChunks,
 };
