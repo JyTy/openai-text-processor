6062e8c2241146736b9a14591806f1e2cc0fc439
Janko Tomsic <janko.tomsic@dlabs.si>
2023-04-12 10:28:42 +0200

    Improved the summary process
    The combined summary logic if response hits the token limit is not yet tested and might not work properly.

--- services/strategies/summariseChatV3.js
+++ services/strategies/summariseChatV3.js
@@ -5,6 +5,7 @@ const openai = require("../openaiClient");
 
 async function chatSummarizev3(inputText) {
   let summary = "";
+  let finalSummary = "";
   const model = "gpt-3.5-turbo";
   let tokensUsed = 0;
   const maxLength = 12000; // experimental apparently this needs to be counted by tiktoken... pyton scrypt ready and utill added but not implemented
@@ -72,45 +73,131 @@ async function chatSummarizev3(inputText) {
       ];
     }
 
+    console.log("\n--------------");
     console.log("processing chunk ", i + 1);
     console.log("chunk length: ", textChunks[i].length);
 
     // console.log("prompt: ", messages[0].content);
     // console.log("prompt: ", messages);
 
-    try {
-      const response = await openai.createChatCompletion({
-        model: model,
-        messages: messages,
-      });
+    const response = await createChatCompletion(messages, model);
 
-      //   summary +=
-      //     "\n\n" +
-      //     "ORIGINAL PROMPT: " +
-      //     textChunks[i] +
-      //     "\n\n" +
-      //     "SUMMARY: " +
-      //     "\n\n";
+    //   summary +=
+    //     "\n\n" +
+    //     "ORIGINAL PROMPT: " +
+    //     textChunks[i] +
+    //     "\n\n" +
+    //     "SUMMARY: " +
+    //     "\n\n";
 
-      assistantResponse = response.data.choices[0].message.content;
-      //   console.log("response: ", assistantResponse);
+    assistantResponse = response.data.choices[0].message.content;
+    //   console.log("response: ", assistantResponse);
+    // console.log("response: ", response.data);
+
+    if (i > 0) {
+      summary += "\n\n";
+    }
+
+    summary += assistantResponse;
+    tokensUsed += response.data.usage.total_tokens;
+    console.log("Total tokens used so far: ", tokensUsed);
+  }
+
+  if (textChunks.length > 1) {
+    const secondPass = [
+      {
+        role: "system",
+        content:
+          "You are a helpful assistant that summarizes text in great detail.",
+      },
+      {
+        role: "user",
+        content:
+          `
+        I'm sending you the summary of a meeting that consists of several inconsistend pieces.
+        I need you to rewrite this summary as a more cohesieve piece. Please make sure you keep
+        all of the detail captured in the summary outtakes, leave nothing out. Also add a list
+        of actions that need to be taken after based on the meeting summary and add it at the endof the summary text.
+        Here are is the summary that needs polishing:` +
+          "\n\n" +
+          summary,
+      },
+    ];
+
+    console.log("\n\n--------------");
+    console.log("processing final pass");
+    // const response = await createChatCompletion(secondPass, model);
+
+    let isComplete = false;
+    let maxIterations = 5;
+    let currentIteration = 0;
+    while (!isComplete && currentIteration < maxIterations) {
+      // console.log("secondPass: ", secondPass);
+      const response = await createChatCompletion(secondPass, model);
+
+      finalSummary += response.data.choices[0].message.content;
       // console.log("response: ", response.data);
 
-      if (i > 0) {
-        summary += "\n\n";
+      console.log("Response token usage: ", response.data.usage);
+
+      // Check if the response contains a completion indicator.
+      // You can change this condition depending on the indicator you want to use.
+      if (response.data.choices[0].finish_reason === "stop") {
+        isComplete = true;
+      } else {
+        console.log(
+          "To long response for a single request, continuing with next pass"
+        );
+        secondPass.push({
+          role: "user",
+          content:
+            "Please continue summarizing the text from where you left off.",
+        });
       }
 
-      summary += assistantResponse;
       tokensUsed += response.data.usage.total_tokens;
-    } catch (error) {
-      console.error("Error generating summary:", error);
-      return null;
+      console.log("Total tokens used so far: ", tokensUsed);
+      currentIteration++;
     }
+
+    // Replace the summary with the final pass if enoug tokens to do a full response,
+    // otherwise just return the unpolished summary
+    // if (response.data.choices.finish_reason === "stop") {
+    //   summary = response.data.choices[0].message.content;
+    // } else {
+    //   console.log(
+    //     "Not enough tokens to do final pass, returning unpolished summary"
+    //   );
+    // }
   }
 
   calculateCost(tokensUsed, model);
 
-  return summary;
+  return finalSummary;
+}
+
+async function createChatCompletion(messages, model) {
+  try {
+    const response = await openai.createChatCompletion({
+      model: model,
+      messages: messages,
+      temperature: 0.2,
+    });
+
+    // console.log(response.data);
+
+    return response;
+  } catch (error) {
+    if (error.response && error.response.status === 429) {
+      console.error(
+        `Rate limit hit. Limit: ${error.response.headers["x-ratelimit-limit"]}, Remaining: ${error.response.headers["x-ratelimit-remaining"]}, Reset: ${error.response.headers["x-ratelimit-reset"]}`
+      );
+      process.exit(1);
+    } else {
+      console.error("Error generating summary:", error);
+      return null;
+    }
+  }
 }
 
 module.exports = {
